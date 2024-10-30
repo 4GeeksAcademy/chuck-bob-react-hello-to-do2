7c361eb6c68fa453f6ba60323318bd0725d3a325
Chuck-Bob <derekaassaf@gmail.com>
2024-10-30 18:25:35 +0000

    sixth

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React , { useState } from "react";
 import Display from "./display";
 import './Styles.css';
+import LoginPage from "./loginpage";
 
 
 
@@ -12,7 +13,8 @@ const Home = () => {
 
 	const [list, setList] = useState(["derek"])
 	const [task, setTask] = useState("")
-
+	const [view, setView] = useState("login-page")
+	const [username, setUsername] = useState("")
 
 	const handleRemove = (indexToRemove) => {
 		setList(list.filter((_, index) => index !== indexToRemove));
@@ -21,8 +23,8 @@ const Home = () => {
 	const addTask = () => setList([...list, task]);
 
 	return (
-		
-		<div className="text-center">
+		view !== "login-page" ? (
+		<div className="text-center">{username}
 
 			<div className="todoInput">
 				<input type="text" value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => {
@@ -39,8 +41,10 @@ const Home = () => {
 			<div className="displayList">
 				{list.map((task, index) => (<Display key={index} task={task} index={index} handleRemove={handleRemove} /> ))}
 			</div>
-		</div>
 			
+		</div>
+		)
+		: (<LoginPage setUsername={setUsername} setView={setView}/>)
 	);
 };
 
