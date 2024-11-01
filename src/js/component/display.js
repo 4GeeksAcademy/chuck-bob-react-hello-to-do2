a243b0924c3cfca96a3df4281237ebfde42a4f13
Chuck-Bob <derekaassaf@gmail.com>
2024-11-01 14:23:39 +0000

    not sure what number

--- src/js/component/display.js
+++ src/js/component/display.js
@@ -1,19 +1,31 @@
-import React , { useState } from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React from "react";
 import PropTypes from "prop-types";
 
-//create your first component
 const Display = (props) => {
-let {task,handleRemove,index} = props;
 	
-
+	let { task, handleRemove, index } = props;
+  
 	return (
-		<div className="list">{task.label}
-        <button onClick= {()=> handleRemove(index)}>X</button>
-        </div>
+	  <div className="list">
+		
+		{task.label} - {task.is_done ? "Done" : "Pending"}
+		
+		{/* Button to remove the task by calling handleRemove and passing the task's index */}
+		<button onClick={() => handleRemove(index)}>X</button>
+		
+		{/* Conditionally render the "Mark as Done" button if the task is not done yet */}
+		{!task.is_done && (
+		  <button onClick={() => props.updateTaskStatus(task)}>Mark as Done</button>
+		)}
+	  </div>
 	);
+  };
+
+Display.propTypes = {
+  task: PropTypes.object.isRequired,
+  handleRemove: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  updateTaskStatus: PropTypes.func.isRequired
 };
 
 export default Display;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,26 +3,22 @@ import Display from "./display";
 import './Styles.css';
 import LoginPage from "./loginpage";
 
-
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 
-	const [list, setList] = useState([""])
-	const [task, setTask] = useState("")
-	const [view, setView] = useState("login-page")
-	const [username, setUsername] = useState("")
+	const [list, setList] = useState([""]);
+	const [task, setTask] = useState("");
+	const [view, setView] = useState("login-page");
+	const [username, setUsername] = useState("");
 
-	const [regUsers, setRegUsers] = useState(["George", "Marjorie", "Derek"])
-
+	const [regUsers, setRegUsers] = useState(["George", "Marjorie", "Derek"]);
 
 	const handleRemove = (indexToRemove) => {
 		setList(list.filter((_, index) => index !== indexToRemove));
 	};
-
 
 	const addTask = (newTask) => {
 		fetch('https://playground.4geeks.com/todo/todos/chuck-bob', {
@@ -34,40 +30,67 @@ const Home = () => {
 			headers: { 'Content-type': 'application/json' }
 		})
 			.then((response) => response.json())
-			.then((jsonifiedData) => getFetch())
-			.catch((err) => console.log(err))
+			.then(() => getFetch())
+			.catch((err) => console.log(err));
 	};
 
-	const getFetch = () => { fetch('https://playground.4geeks.com/todo/users/chuck-bob')
-	.then((response) => response.json())
-	.then((jsonifiedData) => setList(jsonifiedData.todos))
-	.catch((err) => console.log(err));
-	}
+	// Function to fetch and update the task list
+	const getFetch = () => {
+		fetch('https://playground.4geeks.com/todo/users/chuck-bob')
+			.then((response) => response.json())
+			.then((jsonifiedData) => setList(jsonifiedData.todos))
+			.catch((err) => console.log(err));
+	};
 
+	// Function to update a task's is_done status to true
+	const updateTaskStatus = (task) => {
+		const taskId = task.id; 
+ 		fetch(`https://playground.4geeks.com/todo/todos/${taskId}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				label: task.label,
+				is_done: true
+			})
+		})
+		.then(response => response.json())
+		.then(() => getFetch()) // Refresh the list after updating the task
+		.catch(error => console.log(error));
+	};
 
-	useEffect(() => { getFetch()
-	}, []);
-
-
+	useEffect(() => { getFetch() }, []);
 
 	return (
 		view !== "login-page" ? (
 			<div className="text-center">{username}
 				<div className="todoText">TO DO LIST</div>
 				<div className="todoInput">
-					<input type="text" value={task} onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => {
-						if (e.key === 'Enter') {
-							addTask(task);
-						}
-					}} />
+					<input 
+						type="text" 
+						value={task} 
+						onChange={(e) => setTask(e.target.value)} 
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+								addTask(task);
+							}
+						}} 
+					/>
 				</div>
 
 				<div className="addButton">
-					<button onClick={()=>addTask(task)}> add </button>
-				</div >
+					<button onClick={() => addTask(task)}> add </button>
+				</div>
 
 				<div className="displayList">
-					{list.map((task, index) => (<Display key={index} task={task} index={index} handleRemove={handleRemove} />))}
+					{list.map((task, index) => (
+						<Display 
+							key={index} 
+							task={task} 
+							index={index} 
+							handleRemove={handleRemove} 
+							updateTaskStatus={updateTaskStatus} 
+						/>
+					))}
 				</div>
 
 			</div>
